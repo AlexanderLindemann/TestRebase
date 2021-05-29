fbfd60103686d90a5b485ff5dd869c107f8b6deb
Александр Линдеманн <idsmokedried@gmail.com>
2021-05-29 19:26:33 +0300

    Test commit

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+
+
+    public static void main(String[] args) {
+        int a = Integer.parseInt("5");
+    }
+}
---
+++ src/Test2.java
@@ -0,0 +1,11 @@
+public class Test2 {
+    String a;
+
+    public Test2(String a) {
+        this.a = a;
+    }
+
+    public String getA() {
+        return a;
+    }
+}
