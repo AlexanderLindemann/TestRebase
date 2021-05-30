b6b5e8a42cf595210c4048fcfeb7dadc14bbe05b
Александр Линдеманн <idsmokedried@gmail.com>
2021-05-30 21:15:50 +0300

    add new class Person

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,37 @@
 
+//класс билдер, который стряпает человека)
+public class Person {
+    private String name;
+    private int age;
+    private String surname;
+    private int weight;
 
-public class Test4 {
+    private Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public class Builder {
+        private Builder() {
+        }
+
+        public Builder setName(String name) {
+            Person.this.name = name;
+            return this;
+        }
+    }
 }
