c672982ab3e74ddde8f0e4460a5181adcdb84647
Александр Линдеманн <idsmokedried@gmail.com>
2021-05-30 21:30:02 +0300

    create Person class in Test class

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,16 @@ public class Test {
 
 
     private static Test2 test2 = new Test2("5");
+    Person person = Person.newBuilder()
+            .setName("Sasha")
+            .setAge(31)
+            .build();
 
 
     public static void main(String[] args) {
         int a = Integer.parseInt(test2.testMethod(test2.getA()));
         System.out.println(a);
         System.out.println("Hello everyone!");
+        System.out.println();
     }
 }
